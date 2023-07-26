74a0f4b698512a9c8b623fd203694b47aa81e6c6
JoseERBWolf <wolf.jose89@gmail.com>
2023-07-26 01:08:33 -0300

    mostrando os dados no console

--- src/Modelo/Endereco.java
+++ src/Modelo/Endereco.java
@@ -2,4 +2,13 @@ package Modelo;
 
 public record Endereco(String cep, String logradouro, String complemento,
                        String localidade, String uf) {
+
+    @Override
+    public String toString() {
+        return "cep = " + cep  +
+                ", logradouro = " + logradouro +
+                ", complemento = " + complemento +
+                ", localidade = " + localidade +
+                ", uf = " + uf + "\n";
+    }
 }
